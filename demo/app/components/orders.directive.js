c2f06bce85a236ce6f95c49a85d65ad3c4425c04
taras-shkromyda <taras.shkromyda@lezgro.com>
2015-12-04 10:54:32 +0200

    modified main view

--- demo/app/components/orders.directive.js
+++ demo/app/components/orders.directive.js
@@ -1,10 +1,7 @@
 demoApp.directive('orders', function () {
     return {
         restrict: 'E',
-        templateUrl: 'app/components/orders.directive.html',
-        scope: {
-            orders: '=list'
-        }
+        templateUrl: 'app/components/orders.directive.html'        
     };
 });
 
